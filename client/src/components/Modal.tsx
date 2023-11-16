43301e84330c637abd550a01b384296aef012e8c
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2023-11-16 20:37:56 +0300

    animations {Modal}

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { HiOutlineX } from "react-icons/hi";
+import { AnimatePresence, motion } from "framer-motion";
+
+const variants = {
+    visible: {
+        opacity: 1,
+        scale: 1
+    },
+    hidden: {
+        opacity: 0,
+        scale: 2
+    }
+}
 
 type ModalProps = {
     isOpen: boolean
@@ -11,16 +23,23 @@ const Modal = ({ isOpen, children, onClose }: ModalProps) => {
     if (!isOpen) {
         return null;
     };
-    
+
     return (
-        <div className={`w-full h-screen flex items-center justify-center fixed top-0 left-0 z-20 bg-black/30`}>
-            <div className="p-10 relative bg-white border border-gray-light shadow-xl">
-                <button onClick={onClose} className="absolute top-4 right-5 text-gray-strength hover:text-black" aria-label="close" >
-                    <HiOutlineX size={36} />
-                </button>
-                {children}
+        <AnimatePresence>
+            <div className={`w-full h-screen flex items-center justify-center fixed top-0 left-0 z-20 bg-black/30`}>
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={variants}
+                    className="p-10 relative bg-white border border-gray-light shadow-xl"
+                >
+                    <button onClick={onClose} className="absolute top-4 right-5 text-gray-strength hover:text-black" aria-label="close" >
+                        <HiOutlineX size={36} />
+                    </button>
+                    {children}
+                </motion.div>
             </div>
-        </div>
+        </AnimatePresence>
     );
 }
 
